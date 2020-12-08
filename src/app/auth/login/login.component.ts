cf6076b47a0e673933d97cdb8f2684327f5e5a67
kirill-bobr <kirillbobryakov82@gmail.com>
2020-12-08 23:39:46 +0800

    Change name of selector (signin to login), change routing from Auth to Profile

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
-
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-signin',
+  selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss', '../auth.component.scss']
+
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+  constructor(private router: Router) {
+  }
+
+  onSubmit(): void {
+    this.router.navigateByUrl('profile');
+  }
+
+}
